e62b21d03917da8547c981d7325e778b65509751
Hammer <1372710798@qq.com>
2026-06-13 22:37:52 +0800

    Add WeChat verification TXT endpoint at root path

--- xinfan/urls.py
+++ xinfan/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.core.views import verify_txt
 
 urlpatterns = [
+    path('5245070195fc163a6d7e3927ecac8193.txt', verify_txt),
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('accounts/', include('apps.accounts.urls')),
